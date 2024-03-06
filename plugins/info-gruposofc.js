fda15f81901a1e68c00dec86d899a3a20b126d72
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-03-05 21:37:01 -0600

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,28 +3,28 @@ let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*Hola usuario estos son los grupos ofc de Shana-bot*
 
-📎 *Shana-Bot 🈴*
+📎 *CAPIBOT VENTAS 🈴*
  
-https://chat.whatsapp.com/JKTtlxj8K2U4utBtFXhd9Z
+https://chat.whatsapp.com/IkmoGgRSDi46dOTiH1EF74
 
 📎 *Canal ⭐*
 
-https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33
+https://chat.whatsapp.com/IkmoGgRSDi46dOTiH1EF74
 
 ⚙️ *Grupos En Colaboración:*
 
-1 _Shana-Bot & NovaBot_
+1 _CAPIBOT_
 
-https://chat.whatsapp.com/C7g96LhthOb6I8QMye9GrI
+https://chat.whatsapp.com/IkmoGgRSDi46dOTiH1EF74
 
-2 SakuraBot & Shana-Bot
+2 CAPIBOT
 
- https://chat.whatsapp.com/HjtosKShIYcE0xldUFSnkF
+ https://chat.whatsapp.com/IkmoGgRSDi46dOTiH1EF74
 
 ─────────────
 🧿Si se restableció los enlaces comunicate con mi creador usa el comando #creador para comunicarte con el 🌟`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
-/*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n\n\n` + wm, media, 'https://github.com', 'Shana-Bot', null, null, [
+/*conn.sendHydrated(m.chat, str, `CAPIBOT - ASISTENCIA\n\n\n` + wm, media, 'https://github.com', 'CAPI-BOT', null, null, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentassb'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
